f127fe4d9bddacd74dd51a44b2493f7a2d276023
Smile-Lele <zenghongqing123@163.com>
2021-08-13 13:59:49 +0800

    Add: multi thread read video

--- 8_machine_learning/delaunay.py
+++ 8_machine_learning/delaunay.py
@@ -21,25 +21,27 @@ points = [cv.minEnclosingCircle(cnt)[0] for cnt in contours]
 
 points = np.array(points).astype(np.float32)
 
-K = 200
+K = 5
 criteria = (cv.TERM_CRITERIA_MAX_ITER + cv.TERM_CRITERIA_EPS, 10, 1.0)
 ret, labels, centers = cv.kmeans(points, K, None, criteria, attempts=25, flags=cv.KMEANS_RANDOM_CENTERS)
 
 centers = centers.tolist()
 for n, c in enumerate(centers):
+    cv.circle(img, np.uint(c), 8, (0, 0, 255), -1, cv.LINE_8)
+
     pnts = points[labels.ravel() == n]
 
     rect = cv.boundingRect(pnts)
     subdiv = cv.Subdiv2D(rect)
-    [subdiv.insert(p) for p in pnts]
+    subdiv.insert(pnts.tolist())
 
     triangles = subdiv.getTriangleList()
     for t in triangles:
         t = t.reshape(-1, 2).astype(np.int32)
         cv.polylines(img, [t], True, random.choice(COLORS), 1, cv.LINE_8)
 
-    # cv.circle(img, np.uint(c), 8, (0, 0, 255), -1, cv.LINE_8)
 
+img = cv.resize(img, None, fx=0.5, fy=0.5, interpolation=cv.INTER_AREA)
 cv.imshow('', img)
 # cv.imwrite('delaunay.png', img)
 key = cv.waitKey() & 0XFF
