3d317b0c395781606574a15e83db271de9f7515a
Smile-Lele <zenghongqing123@163.com>
2022-04-03 11:52:54 +0800

    Modify camera class

--- 6_video_analysis/camera_uvc_new.py
+++ 6_video_analysis/camera_uvc_new.py
@@ -16,20 +16,36 @@ class CamWriter:
 
 
 class CamReader:
+    class PROP(Enum):
+        CAP_PROP_FRAME = 6601
+
     def __init__(self, device_idx, api=cv.CAP_ANY):
         self.device_idx = device_idx
         self.api = api
+        self.frames = []
         self.cap = cv.VideoCapture(self.device_idx, self.api)
+        print(Fore.CYAN + f'CAM={self.cap.isOpened()}')
         print(Fore.CYAN + f'API={self.cap.getBackendName()}')
         self.cap_props = {eval('cv.' + cap_prop): cap_prop for cap_prop in dir(cv) if cap_prop.startswith("CAP_PROP_")}
-        self.frames = []
 
     def set(self, prop_id, value):
+        if prop_id == self.PROP.CAP_PROP_FRAME:
+            ret = self.cap.set(3, value[0]) and self.cap.set(4, value[1])
+            if not ret or abs(self.cap.get(3) - value[0]) > 1e-3 or abs(self.cap.get(4) - value[1]) > 1e-3:
+                print(Fore.RED + f'SET_CAP_PROP_FRAME:{self.cap.get(3)}x{self.cap.get(4)}')
+                return ret
+            print(Fore.GREEN + f'CAP_PROP_FRAME={self.cap.get(3)}x{self.cap.get(4)}')
+            return ret
+
         ret = self.cap.set(prop_id, value)
-        if not ret or self.cap.get(prop_id) != value:
-            print(Fore.RED + f'SET_{self.cap_props.get(prop_id)}_ERROR:{self.cap.get(prop_id)}')
+        tgt = self.cap.get(prop_id)
+        if prop_id == cv.CAP_PROP_FOURCC:
+            tgt = self.decode_fourcc(tgt)
+
+        if not ret or abs(self.cap.get(prop_id) - value) > 1e-2:
+            print(Fore.RED + f'SET_{self.cap_props.get(prop_id)}:{tgt}')
             return False
-        print(Fore.GREEN + f'{self.cap_props.get(prop_id)}={value}')
+        print(Fore.GREEN + f'{self.cap_props.get(prop_id)}={tgt}')
         return True
 
     def read(self):
@@ -51,10 +67,10 @@ class CamReader:
     def isOpened(self):
         return self.cap.isOpened()
 
-    def decode_fourcc(self, v: str):
+    def decode_fourcc(self, v):
         return ''.join([chr((int(v) >> 8 * i) & 0xFF) for i in range(4)])
 
-    def encode_fourcc(self, v):
+    def encode_fourcc(self, v: str):
         return cv.VideoWriter_fourcc(*v)
 
     def release(self):
@@ -62,25 +78,24 @@ class CamReader:
 
 
 if __name__ == '__main__':
-    cam = CamReader(0, cv.CAP_ANY)
-    cam.set(cv.CAP_PROP_FRAME_WIDTH, 1280)
-    cam.set(cv.CAP_PROP_FRAME_HEIGHT, 720)
-    cam.set(cv.CAP_PROP_FPS, 30)
-    cam.set(cv.CAP_PROP_EXPOSURE, 100)
+    cam = CamReader(0, cv.CAP_DSHOW)
+    cam.set(cam.PROP.CAP_PROP_FRAME, (1280, 720))
+    cam.set(cv.CAP_PROP_FPS, 10)
+    cam.set(cv.CAP_PROP_EXPOSURE, -6)
     cam.set(cv.CAP_PROP_GAMMA, 100),
     cam.set(cv.CAP_PROP_GAIN, 0),
-    cam.set(cv.CAP_PROP_FOURCC, cam.encode_fourcc('XVID'))
+    cam.set(cv.CAP_PROP_FOURCC, cam.encode_fourcc('YUY2'))
+    while cam.isOpened():
+        frame = cam.read()
+        # cam.record('test.mp4')
+        cv.imshow('frame', frame)
+        k = cv.waitKey(5)
+        if k == 27:
+            cv.imwrite('test.bmp', frame)
+            cv.destroyWindow('frame')
+            break
 
-    # while cam.isOpened():
-    #     ret, frame = cam.read()
-    #     # cam.record('test.mp4')
-    #     cv.imshow('frame', frame)
-    #     k = cv.waitKey(5)
-    #     if k == 27:
-    #         cv.destroyWindow('frame')
-    #         break
-
-    cam.capture(60)
-    img = cam.concat()
-    cv.imshow('f', img)
-    cv.waitKey()
+    # cam.capture(60)
+    # img = cam.concat()
+    # cv.imshow('f', img)
+    # cv.waitKey()
