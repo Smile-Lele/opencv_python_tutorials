838111e56100062a3080f00f6320dfe59170f094
Smile-Lele <zenghongqing123@163.com>
2022-07-15 09:45:09 +0800

    format code

--- 3_core_operations/polyfit_pi.py
+++ 3_core_operations/polyfit_pi.py
@@ -1,16 +1,17 @@
-from  mypackage.imUtils import icv
+from mypackage.imUtils import icv
 from matplotlib import pyplot as plt
 from mypackage.fileUtils import myfile
 import numpy as np
 
+
 def read_pnts():
     file = myfile.IFile('pi.json')
     data = file.read()
-    pnts = [(d['Energy'], d['Currents'])for d in data['Table']]
+    pnts = [(d['Energy'], d['Currents']) for d in data['Table']]
     return pnts
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
     pnts = read_pnts()
     coeffs = icv.polyfit(pnts, 3)
     pnts = np.asarray(pnts)
